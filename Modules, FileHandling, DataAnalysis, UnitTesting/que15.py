47a6e35d9c08e85c2d7a33647b5541abdbdc3a96
PRANEETH REDDY GORLA <97269555+gpreddy172@users.noreply.github.com>
2024-05-09 10:27:18 +0530

    Add files via upload

---
+++ Modules, FileHandling, DataAnalysis, UnitTesting/que15.py
@@ -0,0 +1,6 @@
+import datetime #importing datetime module
+first=datetime.date(2024,4,2) 
+second=datetime.date(2024,5,8)
+#passing two dates to the variaables 
+delta=(second-first) #substracting the days remaining between the two days
+print(delta)
